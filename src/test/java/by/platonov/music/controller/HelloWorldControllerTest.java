8a4b71cd1307259e84266103696ac8272f716a5d
Dzmitry Platonov <pldi@mail.ru>
2019-06-07 22:13:21 +0300

    connection pool update

---
+++ src/test/java/by/platonov/music/controller/HelloWorldControllerTest.java
@@ -0,0 +1,17 @@
+package by.platonov.music.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * @author dzmitryplatonov on 2019-06-07.
+ * @version 0.0.1
+ */
+public class HelloWorldControllerTest {
+
+    @Test
+    public void doGet() {
+        assertTrue(true);
+    }
+}
